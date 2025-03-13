c79aa4f663f37ac8c490bc928f3c1f5c71bea10c
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-03-13 11:19:09 +0100

    Made a solution

---
+++ chat_gpt_prep_problems/reveresed_word_order.py
@@ -0,0 +1,12 @@
+"""
+Write a function that reverses the order of words in a given string. Words are separated by one or more spaces. The
+function should return a new string with words in reverse order, but with only a single space separating them.
+"""
+
+
+def reverse_words(s: str) -> str:
+    word_l = s.split()
+    reversed_l = reversed(word_l)
+    return "".join(word.replace(" ", "") + " " for word in reversed_l)
+
+print(reverse_words("  asadfa  sadfasd  "))
